68e5cb0f4e3db722b1e5d8fedc0d7595357c361c
cgd <70825723+cupglassDEV@users.noreply.github.com>
2024-08-22 13:00:40 +0000

    oh finally

--- assets/bio.js
+++ assets/bio.js
@@ -12,19 +12,8 @@ export default {
         "luau",
     ],
     notice: {
-        livevid: `<div class="vids">
-        <iframe width="520" height="300" src="https://www.youtube.com/embed/rONUSWIPhbA?si=aMamh5c3Grt2nqtz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe><br><iframe width="520" height="300" src="https://www.youtube.com/embed/JM_0h1E4Ols?si=rrVSql6wzsxW3gaE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>`,
-        livevidCopyright:
-            "© 2024 CNN Indonesia, Trans Media & Warner Bros. Discovery all rights reserved",
-        title: "someone is trying to bypass smth at my country",
+        title: "huh, its fine now",
         important: true,
-        links: [
-            "https://www.bbc.com/indonesia/articles/ckgwe5qpyzjo",
-            "https://www.metrotvnews.com/read/kM6CaeyG-gibran-bungkam-disinggung-soal-gerakan-peringatan-darurat",
-            "https://nasional.kompas.com/read/2024/08/21/16002441/dpr-anulir-mk-bola-panas-di-kpu-pakar-kita-lihat-membangkang-atau-jaga",
-            "https://nasional.kompas.com/read/2024/08/22/05030411/putusan-mk-untungkan-gibran-diikuti-yang-rugikan-kaesang-diakali?page=all",
-        ],
-        description: `House of Representative (DPR) Indonesia is trying to legalize the Election Law revision (Undang Undang Pilkada) to make our current president's child CAN register to the regional election to be the governor,<br>even though his age is still smaller than the legal age for registering the election at the momment<br><br>The riot action is EXACTLY the same like in Paris not too long ago (1 year ago)<br><br>this issue is pretty confusing tbh because before this the Constitutional Court of Indonesia (Mahkamah Konstitusi) made an Election Law revision to make an easier way (for the content, its still <a href="https://nasional.kompas.com/read/2024/08/22/05030411/putusan-mk-untungkan-gibran-diikuti-yang-rugikan-kaesang-diakali?page=all" class="child">a mystery..</a>) for the upcoming president which is the current president's child too.<br><br>But this law make the current president's another child (whos trying to register to become the governor (which is the same person as i tell you from the start)) harder. So they (DPR) trying to make a revision here`,
-        footnote: "maybe the 98 incident will be repeated again.. idk",
+        description: `House of Representative (DPR) Indonesia already cancelled the revision. Thats great... maybe?`,
     },
 };
